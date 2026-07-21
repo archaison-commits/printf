177b4cf174f0310163bf8d0bd57ba878efd7d51c
Bruno Rechiedei <brechied@c3r4s3.42warsaw.pl>
2026-07-21 21:21:22 +0200

    asd

--- ft_putpointer.c
+++ ft_putpointer.c
@@ -10,27 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	len_ft(unsigned int num)
-{
-	int	len;
 
-	len = 0;
-	if (num == 0)
-		len++;
-	while (num != 0)
-	{
-		len++;
-		num /= 16;
-	}
-	return (len);
-}
-
-void	ft_puthex_lo(unsigned int n)
+void	*ft_puthex_lo(unsigned int n)
 {
-	char	*base;
+	char	*prefix;
+	char	*address;
 	int		len;
 
-	base = "0123456789abcdef";
+	prefix = "0x";
+	address = "0123456789abcdef";
 	if (n > 15)
 		ft_puthex_lo(n / 16);
 	len = len_ft(n);
